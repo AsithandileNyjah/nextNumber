850d9e5b8cd0a013081bd14c26b9825a97f59e14
AsithandileNyjah <atyuluu@gmail.com>
2023-11-22 15:00:36 +0000

    JavaScript functionality

---
+++ JavaScript/index.js
@@ -0,0 +1,18 @@
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+let x = 0;
+
+let btn = document.querySelector('[next]');
+let display = document.querySelector('[seeHere]');
+
+btn.addEventListener('click', nextNum);
+
+function nextNum() {
+    if (x < numbers.length) {
+        let nextNumber = numbers[x];
+        x++;
+        console.log(nextNumber);
+        display.value = nextNumber;
+    } else {
+        x = 0;    
+    }
+}
